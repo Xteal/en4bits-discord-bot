9977f72db16e11c633c0f93ac518762643c064ed
Xteal <kevinra8@gmail.com>
2022-02-13 12:40:58 +0100

    upload bot first time

---
+++ bot.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+const Discord = require("discord.io");
+const logger = require("winston");
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = "debug";
+
+// Initialize Discord Bot
+const bot = new Discord.Client({
+    token: process.env.TOKEN,
+    autorun: true,
+    intent: ['GUILD','GUILD_MESSAGES']
+});
+
+bot.on("ready", function(evt) {
+    logger.info(bot.username + "–(" + bot.id + ")");
+});
+
+bot.on("message", function(user, userID, channelID, message, evt) {
+    if (message.substring(0, 1) == "!") {
+        const args = message.split("!");
+        const cmd = args[1];
+        switch (cmd) {
+            case "live":
+                bot.sendMessage({
+                    to: process.env.LIVECHANNELID,
+                    message: `:red_circle: 4Biters :red_circle: estamos en directo @everyone.
+
+                                Os esperamos en https://twitch.tv/en4bits`
+                })
+        }
+    }
+});
